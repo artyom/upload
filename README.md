2a0dfbbb6f4ffaf6f16c39d99f1c49b18f617232
Artyom Pervukhin <artyom@evasive.ru>
2016-01-30 15:31:45 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,45 @@
+Command upload uploads files to new directory on a remote server via ssh. For
+each command call new randomly-named remote subdirectory is created.
+
+	Usage: upload [flags] file...
+	  -addr string
+		ssh host:port (default "localhost:22")
+	  -dir string
+		remote directory to upload files to (default "/tmp")
+	  -url string
+		remote url base to open after upload
+	  -user string
+		ssh connection username (default "$USER")
+
+Current limitations:
+
+ * only files are supported, not directories;
+ * ssh-agent is used for authentication.
+
+## Advanced usage
+
+Can be used to create basic one-person web-share service:
+
+Consider you have a cheap VPS serving `/var/www` as http://example.com. Create
+directory `/var/www/pub`, then run upload on some files like this:
+
+	upload -addr my-vps:22 -dir /var/www/pub -url http://example.com/pub cats1.gif cats2.gif
+
+This should create randomly-named directory inside `/var/www/pub`, upload both
+files into it, then print directory name to stdout (ex. `/var/www/pub/a1`) and
+open http://example.com/pub/a1 in your browser so you can check it and share
+(your web server is expected to show directory listings for this to work).
+
+For OS X users such things can be integrated into UI with a little help of
+Automator. Run Automator and create a new Service that looks like this:
+
+![Automator action screenshot](https://cbxp.in/c0/automator%20screenshot.png)
+
+Adjust script providing your server details, save this as "Share files on web"
+and you'll get a nice Finder context menu:
+
+![Finder context menu displaying custom action](https://cbxp.in/c0/finder%20context%20menu.png)
+
+## LICENSE
+
+[MIT](http://opensource.org/licenses/MIT)
---
+++ upload.go
@@ -0,0 +1,161 @@
+// Command upload uploads files to new directory on a remote server via ssh. For
+// each command call new randomly-named remote subdirectory is created.
+package main
+
+import (
+	"crypto/rand"
+	"errors"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+	"path"
+	"path/filepath"
+	"sync"
+
+	"github.com/artyom/autoflags"
+	"github.com/pkg/browser"
+	"github.com/pkg/sftp"
+
+	"golang.org/x/crypto/ssh"
+	"golang.org/x/crypto/ssh/agent"
+)
+
+func main() {
+	params := struct {
+		User string `flag:"user,ssh connection username"`
+		Addr string `flag:"addr,ssh host:port"`
+		Dir  string `flag:"dir,remote directory to upload files to"`
+		Url  string `flag:"url,remote url base to open after upload"`
+	}{
+		User: os.Getenv("USER"),
+		Addr: "localhost:22",
+		Dir:  "/tmp",
+	}
+	autoflags.Define(&params)
+	flag.Parse()
+
+	cfg, err := config(params.User)
+	if err != nil {
+		log.Fatal(err)
+	}
+	res, err := upload(params.Addr, params.Dir, cfg, flag.Args())
+	if res != "" {
+		fmt.Println(res)
+		if params.Url != "" {
+			browser.OpenURL(path.Join(params.Url, path.Base(res)))
+		}
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func upload(addr, dir string, config *ssh.ClientConfig, files []string) (string, error) {
+	if len(files) == 0 {
+		return "", errors.New("nothing to upload")
+	}
+	for _, name := range files {
+		if fi, err := os.Stat(name); err != nil {
+			return "", err
+		} else if fi.IsDir() {
+			return "", errors.New("upload of directories not yet implemented")
+		}
+	}
+	conn, err := ssh.Dial("tcp", addr, config)
+	if err != nil {
+		return "", err
+	}
+	defer conn.Close()
+	sc, err := sftp.NewClient(conn)
+	if err != nil {
+		return "", err
+	}
+	defer sc.Close()
+	if di, err := sc.Stat(dir); err != nil {
+		return "", err
+	} else if !di.IsDir() {
+		return "", errors.New("destination path is not a directory")
+	}
+	var dst string
+	for b, i := randBytes(), 1; i < len(b); i++ {
+		dst = path.Join(dir, fmt.Sprintf("%x", b[:i]))
+		if err = sc.Mkdir(dst); err == nil {
+			break
+		}
+	}
+	if err != nil {
+		return "", errors.New("failed to create new random-named directory, probably too many already exist")
+	}
+
+	for _, name := range files {
+		if err := uploadFile(name, dst, sc); err != nil {
+			return dst, err
+		}
+	}
+	return dst, nil
+}
+
+func uploadFile(name, dir string, sc *sftp.Client) error {
+	f, err := os.Open(name)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	of, err := sc.Create(path.Join(dir, filepath.Base(name)))
+	if err != nil {
+		return err
+	}
+	defer of.Close()
+	bufp := copyBufPool.Get().(*[]byte)
+	defer copyBufPool.Put(bufp)
+	if _, err := io.CopyBuffer(of, f, *bufp); err != nil {
+		return err
+	}
+	return of.Close()
+}
+
+func randBytes() []byte {
+	b := make([]byte, 10)
+	if _, err := rand.Read(b); err != nil {
+		panic(err)
+	}
+	return b
+}
+
+func config(user string) (*ssh.ClientConfig, error) {
+	agentConn, err := net.Dial("unix", os.Getenv("SSH_AUTH_SOCK"))
+	if err != nil {
+		return nil, err
+	}
+	// not closing agentConn here because it should be left open for use in
+	// ssh.ClientConfig
+	sshAgent := agent.NewClient(agentConn)
+	signers, err := sshAgent.Signers()
+	if err != nil {
+		agentConn.Close()
+		return nil, err
+	}
+	config := &ssh.ClientConfig{
+		User: user,
+		Auth: []ssh.AuthMethod{ssh.PublicKeys(signers...)},
+	}
+	return config, nil
+}
+
+func init() {
+	log.SetFlags(0)
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage: %s [flags] file...\n", os.Args[0])
+		flag.PrintDefaults()
+	}
+}
+
+var copyBufPool = sync.Pool{
+	New: func() interface{} {
+		b := make([]byte, 256*1024)
+		return &b
+	},
+}
